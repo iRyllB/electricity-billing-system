b278c86591a2398cd4b6c318cecc25c89a25abad
k4snc <jabien.ramila14@gmail.com>
2024-12-08 00:19:38 +0800

    Update test.py
    sa additional charges, wapa na edit ang sa pag generate pdf part. dele ko kabalog layout ug tarong TT

--- source/test.py
+++ source/test.py
@@ -1,157 +1,84 @@
-import re
-
-#DISREGARD NI DAPIT
-
-def print_banner(title):
-    print("\n" + "=" * 50)
-    print(f"{title.center(50)}")
-    print("=" * 50)
-
-def print_separator():
-    print("-" * 50)
-
-def print_message(message):
-    print("\n" + f"{message}".center(50))
-    print()
-
-print_banner("Welcome to USTP OmniCharge CDO")
-
-
-
-
-
-
-# SUGOD DANI NAKO NAG MODIFY
-class ElectricityBillingSystem:
-    def __init__(self):
-        self.customers = {}
-        self.account_no_counter = 1
-
-    def create_account(self, username, email, first_name, last_name, password, phone_no, address):
-        account_no = self.account_no_counter
-        self.account_no_counter += 1
-        new_customer = Customer(account_no, first_name, last_name, email, password, phone_no, address)
-        self.customers[account_no] = new_customer
-        return account_no
-
-    def log_in(self, username, password):
-        for customer in self.customers.values():
-            if customer.first_name == username and customer.password == password:
-                return customer
-        return None
+def pay_bill_menu(customer):
+    TAX_RATE = 12  # VAT rate
+    print_banner("Pay Your Bill")
+    data = pd.read_csv("random_kwh_consumption_due_dates.csv")
+    random_row = data.sample(n=1).iloc[0]
+    units = random_row['kWh_Consumed']
+    due_date = random_row['Due_Date']
+    exit_loop = False
+    
+    generation_charge = 11.42 #bill rate
+    transmission_charge = 0.8786
+    sysloss_charge = 1.045
+    distribution_charge = 0.4613
+    supply_charge = 0.5376
+    metering_charge = 0.3205
+    rp_tax_provision = 0.0105
+    franchi_tax_cur = 0.0022
+    business_tax_cur = 0.0085
+    
+    # Calculate the total bill using generation charge as the base
+    total_bill = units * generation_charge
+    
+    total_bill += (transmission_charge + sysloss_charge +
+                   distribution_charge + supply_charge + metering_charge +
+                   rp_tax_provision + franchi_tax_cur + business_tax_cur)
+    
+    total_bill_with_vat = total_bill * (1 + TAX_RATE / 100)
+    
+    payment_date = datetime.now().date()
+    next_due_date = (datetime.strptime(due_date, '%Y-%m-%d') + timedelta(days=30)).date()
 
 
-
-class Customer:
-    def __init__(self, account_no, first_name, last_name, email, password, phone_no, address):
-        self.account_no = account_no
-        self.first_name = first_name
-        self.last_name = last_name
-        self.email = email
-        self.password = password
-        self.phone_no = phone_no
-        self.address = address
-        self.bills = []
-
-
-# Validation SA PASSWORD IF NA MEET ANG REQUIRMENTS
-def is_valid_password(password):
-    if (len(password) >= 8 and 
-        re.search(r"[a-z]", password) and 
-        re.search(r"[A-Z]", password) and 
-        re.search(r"[0-9]", password) and 
-        re.search(r"[!@#$%^&*(),.?\":{}|<>]", password)):
-        return True
-    return False
-
-
-#VALIDATION SA PHONE NUMBRE IF NUMBERIC AND 11 DIGITS
-def is_valid_phone_number(phone_no):
-    return phone_no.isdigit() and len(phone_no) == 11
-
-
-# PARA PREVENT SA DUPLICATED ACCOUNTS
-def check_if_account_exists(username, email, existing_accounts):
-    for customer in existing_accounts.values():
-        if customer.first_name == username or customer.email == email:
-            return True
-    return False
-
-
-def create_account(system):
-    print_banner("Create Your Account")
-    print("Type 'back' at any time to return to the main menu.")
+    print("\n---------------- Bill Calculation ----------------")
+    print(f"{'Units Consumed':<27}: {units} kWh")
+    
+    # Print charges
+    print("\nGeneration:")
+    print(f"  {'Genr System':<25}: {generation_charge:<10.2f}")
+    
+    print("Transmission:")
+    print(f"  {'Trans System':<25}: {transmission_charge:<10.4f}")
+    print(f"  {'SysLoss Chg':<25}: {sysloss_charge:<10.3f}")
+    
+    print("Distribution:")
+    print(f"  {'Dist System':<25}: {distribution_charge:<10.4f}")
+    
+    print("Supply Charges:")
+    print(f"  {'Supp System':<25}: {supply_charge:<10.4f}")
+    
+    print("Metering Charges:")
+    print(f"  {'Meter System':<25}: {metering_charge:<10.4f}")
+    
+    print("Universal Charges:")
+    print(f"  {'RP Tax Provision':<25}: {rp_tax_provision:<10.4f}")
+    print(f"  {'Franchi Tax Cur':<25}: {franchi_tax_cur:<10.4f}")
+    print(f"  {'Business Tax Cur':<25}: {business_tax_cur:<10.4f}")
+    
+    print(f"\n{'VAT (12%)':<27}: {total_bill_with_vat - total_bill:<10.2f}")
+    print(f"{'Total Bill':<27}: {total_bill_with_vat:<10.2f}")
+    print(f"{'Payment Date':<27}: {payment_date}")
+    print(f"{'Due Date':<27}: {due_date}")
+    print(f"{'Next Due Date':<27}: {next_due_date}")
+    print_separator()
 
     while True:
-        username = input("Enter your username: ").strip()
-        if username.lower() == 'back':
-            return
-        email = input("Enter your email: ").strip()
-        if email.lower() == 'back':
-            return
-        
-        #TO PREVENT ANY DUPLICATED ACCOUNT
-        if check_if_account_exists(username, email, system.customers):
-            print_message("Error: Username or email already exists. Please choose a different one.")
-        elif username and email:
+        print("1. Confirm Payment")
+        print("2. Go Back")
+        confirm = input("Enter your choice: ").lower()
+        if confirm == '1':
+            new_bill = Bill(units, total_bill_with_vat, payment_date, due_date, next_due_date)
+            customer.bills.append(new_bill)
+            
+            generate_receipt_pdf(customer, units, total_bill_with_vat, payment_date, TAX_RATE, due_date, next_due_date)
+            
+            print_message("Payment Successful! Thank you for using our service.")
+            exit_loop = True
+            break
+        elif confirm == '2':
+            print_message("Payment canceled.")
+            exit_loop = True
             break
         else:
-            print_message("Error: Username and email cannot be blank or just spaces. Please try again.")
-
-    # First name input
-    while True:
-        first_name = input("Enter your first name: ").strip()
-        if first_name.lower() == 'back':
-            return
-        if first_name: 
-            break
-        print_message("Error: First name cannot be blank or just spaces. Please try again.")
-    
-    # Last name input
-    while True:
-        last_name = input("Enter your last name: ").strip()
-        if last_name.lower() == 'back':
-            return
-        if last_name: 
-            break
-        print_message("Error: Last name cannot be blank or just spaces. Please try again.")
-    
-    # Password input
-    while True:
-        password = input("Enter your Passkey: ").strip()
-        if password.lower() == 'back':
-            return
-        if is_valid_password(password): 
-            break
-        print_message("Error: Password must be at least 8 characters long and contain a mix of upper/lowercase letters, numbers, and special characters. Please try again.")
-
-    # VALIDATION SA PHONE NUMBER IF NUMERIC AND 11 DIGITS
-    while True:
-        phone_no = input("Enter your phone number: ").strip()
-        if phone_no.lower() == 'back':
-            return
-        if is_valid_phone_number(phone_no): 
-            break
-        print_message("Error: Phone number must be exactly 11 digits. Please try again.")
-
-    # Address input
-    while True:
-        address = input("Enter your address: ").strip()
-        if address.lower() == 'back':
-            return
-        if address: 
-            break
-        print_message("Error: Address cannot be blank or just spaces. Please try again.")
-
-    #ERROR HANDLING 
-    try:
-        account_no = system.create_account(username, email, first_name, last_name, password, phone_no, address)
-        print_message(f"Account Created Successfully!\nYour Account Number: {account_no}")
-    except Exception as e:
-        print_message(f"Error: {e}")
-
-
-#CHECKING 
-system = ElectricityBillingSystem()
-
-create_account(system)
+            print("Invalid input, 1 - 2 only")
+            continue
